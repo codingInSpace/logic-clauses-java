b643e38179ae6684ad91c49d3608221b1e22c818
codingInSpace <jonathan.grangien@gmail.com>
2017-05-15 14:46:33 +0200

    Add jar file that runs package with arguments

--- src/logics/Main.java
+++ src/logics/Main.java
@@ -9,8 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Clause> clauses = new ArrayList<>();
-        clauses.add(new Clause("A u -B u C"));
-        clauses.add(new Clause("-A u B"));
+
+        for (String s : args)
+            clauses.add(new Clause(s));
+
+        // Dev
+        if (args.length == 0) {
+            clauses.add(new Clause("A u -B u C"));
+            clauses.add(new Clause("-A u B"));
+        }
 
         KnowledgeBase kb = new KnowledgeBase(clauses);
 
